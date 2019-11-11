8534abbb1dab7d91b9f262714deac2a37315fda6
Zakari <46904443+aaz000966@users.noreply.github.com>
2019-11-10 20:29:15 -0600

    UPDATED LAYOUT

---
+++ UserLayout1/Search.php
@@ -0,0 +1,59 @@
+<?php
+
+//=======================Get variables======================
+$link = mysqli_connect('mmohzary74735.ipagemysql.com', 'bearth1', '9630','bearthworld');
+if(mysqli_connect_errno()){
+    return  $result['success']= "Error (1): Could not connect to server now. Please try agin later.";
+    exit();
+}
+$result = array();
+if(!$_GET['Type']&& !$_GET['Zip']){
+    return  $result['success'] = 'Error (2): campus and permit information are not available.';
+}else{
+
+    $result['success'] = search($_GET['Type'],$_GET['Zip']);
+}
+
+//=========================Callback code=====================
+if(array_key_exists('callback', $_GET)){
+    header('Content-Type: text/javascript; charset=utf8');
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Max-Age: 3628800');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+    $callback = $_GET['callback'];
+    echo $callback.'('.json_encode($result).');';
+}else{
+// normal JSON string
+    header('Content-Type: application/json; charset=utf8');
+    echo json_encode($result);
+}
+
+//=======================Connect to database====================
+
+function  search($type,$zip){
+    $coursesListData = " ";
+    global $link;
+
+    if (!$link) {
+
+        return  $result['success'] = "Error (3): Could not connect to server now. Please try agin later.";
+
+    }else{
+        $query = " SELECT * FROM courses WHERE 1";
+        $qresult = mysqli_query($link, $query);
+        if(!$qresult || mysqli_num_rows($qresult)== 0){
+            return  $result['success'] = "Error (4): Could not reterive your data. Please try agin later.";
+        }else{
+            while ($row = mysqli_fetch_assoc($qresult)){
+                $coursesListData .= "<ul class='app-list'><label>".$row['Name']."</label><li>Data: ". $row['Date']."</li> <li>From: ". $row['From']."</li> <li>To: ". $row['To']."</li> </ul>";
+            }
+
+        }
+
+    }
+    return  $result['success'] = $coursesListData;
+
+}
+
+?>
